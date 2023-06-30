fc182d3bb0c7adc3ea32008dac9acae0ddc43e77
AsadoREDACTADO <sebarrantes@outlook.com>
2023-06-30 13:54:45 -0500

    WebApi

---
+++ WebApi/Services/IProducto.cs
@@ -0,0 +1,13 @@
+﻿using WebApi_D1M.Models;
+
+namespace WebApi_D1M.Services
+{
+    public interface IProducto
+    {
+        IEnumerable<Producto> GetAllProducts();
+        Producto ProductoById (int id);
+        void add(Producto producto);
+        void update(Producto p);
+        void delete(int id);
+    }
+}
